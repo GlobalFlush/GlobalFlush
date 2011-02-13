885ddd3e953938ea90bc09c00c20d43d9bf714b3
tellingeric <tellingeric@hotmail.com>
2011-02-13 14:47:44 -0500

    liul6 - First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalflush::Application.config.secret_token = '46b0139ec964ce8d267d67c496e06eb05874040f6cef34dec25d4d61e70ff38a7d9d4160756bf1c17e2ee62087356f5e78d7c91d64054f66db4cd954db4d02d3'
