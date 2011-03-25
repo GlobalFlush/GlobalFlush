fabc53744d99f45187d940f3495fdf3f1b3081e4
unknown <liul6@.(none)>
2011-03-25 14:03:45 -0400

    liul6 - add browse page, using bathroom controller#index

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -1,7 +1,11 @@
 class BathroomsController < ApplicationController
+
+  def index
+    @bathrooms = Bathroom.find(:all)
+  end
+
   def show
     @bathroom = Bathroom.find(params[:id])
-    
   end
 
   
