1c80a8f72c281317eed240da3bb10e878cbde2f5
jinz <gong.jinz@gmail.com>
2011-04-03 15:37:07 -0400

    rating javascript still in progress

--- db/migrate/20110221211524_create_ratings.rb
+++ db/migrate/20110221211524_create_ratings.rb
@@ -1,5 +1,11 @@
 class CreateRatings < ActiveRecord::Migration
   def self.up
+	#create the attributes for a rating object: 
+	#bathroom id is the bathroom the rating is for, 
+	#smell is the rating for smelliness, 
+	#clean is the rating for cleanliness, 
+	#overall is the overall rating for the bathroom
+	#timestamps is the date and time of when the object was created
     create_table :ratings do |t|
       t.integer :bathroom_id
       t.integer :smell, :null => false
