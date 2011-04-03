fdf82cf7b91225b4cac316c15130f060f492ff76
unknown <liul6@.(none)>
2011-04-03 14:59:37 -0400

    liul6 - added multiple attr and photos creation fields for bathroom creation, added address div in bathroom show page

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ActiveRecord::Base
   belongs_to :bathroom
+
+  def to_s
+    inside_location + ', ' + street_address + ', ' + city + ', ' + state + ', ' + zip + ', ' + country
+  end
 end
