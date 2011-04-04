c2fa76615e5cbadfd41bf1e4bac36dfa937bf6ce
jinz <gong.jinz@gmail.com>
2011-04-03 20:27:58 -0400

    overall rating is displayed on top right but vertically...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,12 @@ class CommentsController < ApplicationController
     
     def new
 		@comment = Comment.new
-		@rating = Rating.new
+		#@rating = Rating.new
     end
 	
     def create
         @comment = Comment.new(params[:comment])
-		@rating = Rating.new(params[:rating])
+		#@rating = Rating.new(params[:rating])
         @comment.user_id = @current_user.id
         if @comment.save
             redirect_to :back
