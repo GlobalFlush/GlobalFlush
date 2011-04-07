9fdde36f30a89aaed6d953b054a55bda22c3d42a
Joe Valerio <agentepsilon0@gmail.com>
2011-04-06 20:29:43 -0400

    Added documentation to comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,17 @@ class CommentsController < ApplicationController
     
     before_filter :require_user
 	
-    # GET /comments/
+    # GET /comments
+    # Redirect user back or to the homepage
 	def index
 		redirect_to :back
     rescue ActionController::RedirectBackError
         redirect_to root_path
 	end
 	
+    # POST /bathrooms/1
+    # Create a new post based on input and current user id.
+    # Return user to previous page.
     def create
         @rating = Rating.new(:bathroom_id => params[:comment][:bathroom_id], :smell => params[:comment][:smell], :clean => params[:comment][:clean], :overall => params[:comment][:overall])
 		@comment = Comment.new(:bathroom_id => params[:comment][:bathroom_id], :user_id => params[:comment][:user_id], :body => params[:comment][:body], :rating_id => @rating.id)
