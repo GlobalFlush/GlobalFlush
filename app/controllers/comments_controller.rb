af0927ea9f168ba8faf7014b59c649fa5f14604a
Joe Valerio <agentepsilon0@gmail.com>
2011-04-27 18:09:27 -0400

    Comment controller does not get user parameter from the bathroom page.  Removed parameter from line 18.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   # Return user to previous page.
   def create
     @rating          = Rating.new(:bathroom_id => params[:comment][:bathroom_id], :smell => params[:comment][:smell], :clean => params[:comment][:clean], :overall => params[:comment][:overall])
-    @comment         = Comment.new(:bathroom_id => params[:comment][:bathroom_id], :user_id => params[:comment][:user_id], :body => params[:comment][:body], :rating_id => @rating.id)
+    @comment         = Comment.new(:bathroom_id => params[:comment][:bathroom_id],  :body => params[:comment][:body], :rating_id => @rating.id)
 
     @comment.user_id = @current_user.id
     if @comment.save and @rating.save
